d8f12d04099b7fdbb714e4d729e0eb6d48e55bf1
rahul <durshinapally.r@northeastern.edu>
2024-09-19 20:00:22 -0400

    Assignment 1 Pushed

---
+++ Assignment_1/src/model/Product.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rahul
+ */
+public class Product {
+    String FirstName;
+    String LastName;
+    String Email;
+    String Phone;
+    String LicenseNumber;
+    String LicenseState;
+    String SSN;
+    String Address1;
+    String Address2;
+    String City;
+    String State;
+    String Zip;
+    String TelephoneNumber;
+    String FaxNumber;
+    String Languages;
+    String DOB;
+    String Nationality;
+    String PassportNumber;
+    String Occupation;
+    String BloodType;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getLicenseNumber() {
+        return LicenseNumber;
+    }
+
+    public void setLicenseNumber(String LicenseNumber) {
+        this.LicenseNumber = LicenseNumber;
+    }
+
+    public String getLicenseState() {
+        return LicenseState;
+    }
+
+    public void setLicenseState(String LicenseState) {
+        this.LicenseState = LicenseState;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getAddress1() {
+        return Address1;
+    }
+
+    public void setAddress1(String Address1) {
+        this.Address1 = Address1;
+    }
+
+    public String getAddress2() {
+        return Address2;
+    }
+
+    public void setAddress2(String Address2) {
+        this.Address2 = Address2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getLanguages() {
+        return Languages;
+    }
+
+    public void setLanguages(String Languages) {
+        this.Languages = Languages;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String Nationality) {
+        this.Nationality = Nationality;
+    }
+
+    public String getPassportNumber() {
+        return PassportNumber;
+    }
+
+    public void setPassportNumber(String PassportNumber) {
+        this.PassportNumber = PassportNumber;
+    }
+
+    public String getOccupation() {
+        return Occupation;
+    }
+
+    public void setOccupation(String Occupation) {
+        this.Occupation = Occupation;
+    }
+
+    public String getBloodType() {
+        return BloodType;
+    }
+
+    public void setBloodType(String BloodType) {
+        this.BloodType = BloodType;
+    }
+
+    
+    
+}
