72a603a40496dcaeb2baefe54e9ee23a70a40e20
rahul <durshinapally.r@northeastern.edu>
2024-10-06 12:43:29 -0400

    Commit 1

---
+++ Assignment_2/src/model/Person.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rahul
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Address homeAddress;
+    private Address workAddress;
+
+    public Person(String firstName, String lastName, Address homeAddress, Address workAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.homeAddress = homeAddress;
+        this.workAddress = workAddress;
+    
+}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(Address workAddress) {
+        this.workAddress = workAddress;
+    }
+}
